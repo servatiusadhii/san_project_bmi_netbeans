58eea54ad15d9c4c8decd08fd9d2f309d2a3afd5
servatiusadhii <servatiusadhi13@gmail.com>
2021-02-11 21:59:58 +0700

    uploaded at 11/02/2021

---
+++ src/program_bmi/Program_BMI.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package program_bmi;
+
+/**
+ *
+ * @author Servatius
+ */
+public class Program_BMI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login_Frame Laund=new Login_Frame();
+        Laund.setVisible(true);
+        
+    }
+    
+}
